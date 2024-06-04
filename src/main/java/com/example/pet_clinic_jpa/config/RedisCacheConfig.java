13b8efe2b20f66ae2c6f93f6d49d656138b557ed
Vladislav <v.r.ryazantsev@gmail.com>
2024-06-04 10:16:33 +0300

    add pool redis pool config

--- src/main/java/com/example/pet_clinic_jpa/config/RedisCacheConfig.java
+++ src/main/java/com/example/pet_clinic_jpa/config/RedisCacheConfig.java
@@ -1,6 +1,8 @@
 package com.example.pet_clinic_jpa.config;
 
 import com.example.pet_clinic_jpa.domain.Pet;
+import com.example.pet_clinic_jpa.domain.Visit;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +14,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
+import java.util.List;
 
 @Configuration
 @EnableCaching
@@ -20,6 +23,7 @@ public class RedisCacheConfig {
     public static final String OWNER_CACHE = "owner_cache";
     public static final String PET_CACHE = "pet_cache";
     public static final String VISIT_CACHE = "visit_cache";
+    public static final Duration VISIT_CACHE_DURATION = Duration.ofMinutes(30);
 
     @Bean
     CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
@@ -27,16 +31,23 @@ public class RedisCacheConfig {
                 .fromConnectionFactory(connectionFactory)
                 .withCacheConfiguration(OWNER_CACHE, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(60)))
                 .withCacheConfiguration(PET_CACHE, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(30)))
-                .withCacheConfiguration(VISIT_CACHE, RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(30)))
+                .withCacheConfiguration(VISIT_CACHE, RedisCacheConfiguration.defaultCacheConfig().entryTtl(VISIT_CACHE_DURATION))
                 .build();
     }
 
     @Bean
-    RedisTemplate<String, Pet> redisPetTemplate(RedisConnectionFactory connectionFactory){
+    RedisTemplate<String, Pet> redisPetTemplate(RedisConnectionFactory connectionFactory) {
         var template = new RedisTemplate<String, Pet>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         return template;
     }
 
+    @Bean
+    RedisTemplate<String, List<Visit>> redisVisitTemplate(RedisConnectionFactory connectionFactory) {
+        var template = new RedisTemplate<String, List<Visit>>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        return template;
+    }
 }
--- src/main/java/com/example/pet_clinic_jpa/service/impl/VisitServiceImpl.java
+++ src/main/java/com/example/pet_clinic_jpa/service/impl/VisitServiceImpl.java
@@ -1,34 +1,54 @@
 package com.example.pet_clinic_jpa.service.impl;
 
+import com.example.pet_clinic_jpa.config.RedisCacheConfig;
 import com.example.pet_clinic_jpa.domain.Visit;
 import com.example.pet_clinic_jpa.repo.PetJpaRepository;
 import com.example.pet_clinic_jpa.repo.VisitJpaRepository;
 import com.example.pet_clinic_jpa.service.VisitService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StopWatch;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class VisitServiceImpl implements VisitService {
 
     private final VisitJpaRepository repository;
-
     private final PetJpaRepository petJpaRepository;
+    private final RedisTemplate<String, List<Visit>> redisTemplate;
+    private static final String VISIT_KEY = RedisCacheConfig.VISIT_CACHE + "::";
 
     @Override
     @Transactional(readOnly = true)
+    @Cacheable(value = RedisCacheConfig.VISIT_CACHE, key = "#id")
     public Collection<Visit> getVisitsByPetIdentifier(Long id) {
         return repository.getVisitByPetIdentifier(id);
     }
 
+
     @Override
     @Transactional
     public Visit createVisit(Visit visit) {
+        var stopWatch = new StopWatch();
         var pet = petJpaRepository.getReferenceById(visit.getPet().getId());
         visit.setPet(pet);
-        return repository.save(visit);
+        var saved = repository.save(visit);
+        var visits = redisTemplate.opsForValue().get(VISIT_KEY + pet.getId());
+        if (Objects.isNull(visits) || visits.isEmpty()) {
+            visits = new ArrayList<>();
+        }
+        visits.add(saved);
+        redisTemplate.opsForValue().set(VISIT_KEY + pet.getId(), visits, RedisCacheConfig.VISIT_CACHE_DURATION);
+        return saved;
     }
 }
